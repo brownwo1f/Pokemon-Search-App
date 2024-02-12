e8e407a4fb9b4f40778fb1fdf00a0cd85e4258af
Deepak Chauhan <deepak858783@gmail.com>
2024-02-12 21:09:20 +0530

    fixed bugs

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-const searchInput = document.getElementById("search-input");
-const searchForm = document.getElementById("search-form");
-const pokeHP = document.getElementById("hp");
-const pokeAttack = document.getElementById("attack");
-const pokeDefence = document.getElementById("defence");
-const pokeSpecAttack = document.getElementById("special-attack");
-const pokeSpecDefence = document.getElementById("special-defence");
-const pokeSpeed = document.getElementById("speed");
-const pokeName = document.getElementById("pokemon-name");
-const pokeId = document.getElementById("pokemon-id");
-const pokeWeight = document.getElementById("weight");
-const pokeHeight = document.getElementById("height");
-const pokeImg = document.getElementById("img");
+const pokemonID = document.getElementById('pokemon-id');
+const pokemonName = document.getElementById('pokemon-name');
+const spriteContainer = document.getElementById('sprite-container');
+const types = document.getElementById('types');
+const height = document.getElementById('height');
+const weight = document.getElementById('weight');
+const hp = document.getElementById('hp');
+const attack = document.getElementById('attack');
+const defense = document.getElementById('defense');
+const specialAttack = document.getElementById('special-attack');
+const specialDefense = document.getElementById('special-defense');
+const speed = document.getElementById('speed');
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
 
+const getPokemon = async () => {
+  try {
+    const pokemonNameOrId = searchInput.value.toLowerCase();
+    const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokemonNameOrId}`);  // //here the await keyword is waiting for the fetch method to resolve the promise by bringing the pokemon data
+    const data = await response.json();   //// converting the reponse object to meaningful json data similar to the [ then((data) => data.json()) ]
 
-const pokemonInfo = async () => {
-    try{
-        const pokeNameId = searchInput.value.toLowerCase();
-        const response = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${pokeNameId}`);          //here the await keyword is waiting for the fetch method to resolve the promise by bringing the pokemon data
-        const data = await response.json();                                                                                // converting the reponse object to meaningful json data similar to the [ then((data) => data.json()) ]
-        console.log(data);
-        console.log(data.stats[2].base_stat)
-        pokeName.textContent = `${data.name.toUpperCase()}`;
-        pokeId.textContent = `#${data.id}`;
-        pokeWeight.textContent = `${data.weight}`;
-        pokeHeight.textContent = `${data.height}`;
-        pokeImg.innerHTML = `<img id="sprite" src="${data.sprites.front_shiny}" alt="${data.name}">`;
+    pokemonName.textContent = `${data.name.toUpperCase()}`;
+    pokemonID.textContent = `#${data.id}`;
+    weight.textContent = `${data.weight}`;
+    height.textContent = `${data.height}`;
+    spriteContainer.innerHTML = `<img id="sprite" src="${data.sprites.front_default}" alt="${data.name} front default sprite">`;
+    hp.textContent = data.stats[0].base_stat;
+    attack.textContent = data.stats[1].base_stat;
+    defense.textContent = data.stats[2].base_stat;
+    specialAttack.textContent = data.stats[3].base_stat;
+    specialDefense.textContent = data.stats[4].base_stat;
+    speed.textContent = data.stats[5].base_stat;
+    types.innerHTML = data.types.map(obj => `<span>${obj.type.name}</span>`).join('');
 
-        pokeHP.textContent = data.stats[0].base_stat;
-        pokeAttack.textContent = data.stats[1].base_stat;
-        pokeDefence.textContent = data.stats[2].base_stat;
-        pokeSpecAttack.textContent = data.stats[3].base_stat;
-        pokeSpecDefence.textContent = data.stats[4].base_stat;
-        pokeSpeed.textContent = data.stats[5].base_stat;
-        
-    }catch(err){
-        resetTable();
-        alert('Pokémon not found');  // if the pokemon is not found 404 not found
-    }   
-}
+  } catch (err) {
+    resetDisplay();
+    alert('Pokémon not found');
+  }
+};
 
-const resetTable = () => {
-    const sprite = document.getElementById('sprite');
-    if (sprite) sprite.remove();
-  
-    pokeName.textContent = '';
-    pokeId.textContent = '';
-    pokeHeight.textContent = '';
-    pokeWeight.textContent = '';
-    pokeHP.textContent = '';
-    pokeAttack.textContent = '';
-    pokeDefence.textContent = '';
-    pokeSpecAttack.textContent = '';
-    pokeSpecDefence.textContent = '';
-    pokeSpeed.textContent = '';
-  };
+const resetDisplay = () => {
+  const sprite = document.getElementById('sprite');
+  if (sprite) sprite.remove();
+  pokemonName.textContent = '';
+  pokemonID.textContent = '';
+  types.innerHTML = '';
+  height.textContent = '';
+  weight.textContent = '';
+  hp.textContent = '';
+  attack.textContent = '';
+  defense.textContent = '';
+  specialAttack.textContent = '';
+  specialDefense.textContent = '';
+  speed.textContent = '';
+};
 
 searchForm.addEventListener('submit', e => {
-    e.preventDefault();
-    pokemonInfo();
-  });
+  e.preventDefault();
+  getPokemon();
+});
